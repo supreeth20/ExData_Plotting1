2b393ed8a5c2261d2a71274cc116181ba2f8062e
supreeth20 <60085099+supreeth20@users.noreply.github.com>
2020-06-03 14:32:56 +0530

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ plot2 <- function(){
   data$Date = as.Date(data$Date,"%d/%m/%Y")
   data$Time = hms(data$Time)
   dat = data.frame(lapply(data[-c(1,2,dim(data)[1])],function(x){as.numeric(x)}))
-  data[,-c(1,2)] = dat
+  data[,-c(1,2,dim(data)[1])] = dat
   data = subset(data,Date == "2007-02-01" | Date =="2007-02-02")
   png('plot2.png',width=480,height=480)
   plot(data$timedate,data$Global_active_power,type='l',xlab="Day", ylab="Global Active Power (kilowatts)")
